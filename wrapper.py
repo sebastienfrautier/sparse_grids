f8decb286aefe315465c4fc6e0f7aeb2f1823e67
kaibrusch <kaibrusch@gmail.com>
2017-11-08 19:50:38 +0100

    wrapper added with little error handling

--- wrapper.py
+++ wrapper.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import call,CalledProcessError
 import datetime
 from os import listdir
 
@@ -7,10 +7,13 @@ path_to_config = "/sparse_grid/configs/"
 file_names = listdir(path_to_config)
 
 for ele in file_names:
-    start_time =  datetime.datetime.now()
-    call(["./simplemd", path_to_config+ele])
-    end_time = datetime.datetime.now()
-    times.append((ele,end_time - start_time))
-    call(["./simplemd", path_to_config + ele])
-
-print times
+    try:
+        start_time =  datetime.datetime.now()
+        call(["./simplemd", path_to_config+ele])
+        end_time = datetime.datetime.now()
+        times.append((ele,(end_time - start_time).total_seconds()))
+        call(["./simplemd", path_to_config + ele])
+    except CalledProcessError as exc:
+        times.append((ele, "fail"))
+    else:
+        times.append((ele, "else"))
