cd2050957b97dd248bed7b14869b17e1b709b7f3
rob brown <rab170@pitt.edu>
2017-12-07 13:35:25 +0100

    generalize config generator to handle multiple dimensionality p-spaces

--- configGenerator_2D.py
+++ configGenerator_2D.py
@@ -10,17 +10,15 @@ BASE_DIR = os.getcwd()      # if we get amibitous, this could become a command l
 output_folder = 'configs'   # this too
 
 # these are defautls which are used if certain parameters aren't defined for generateConfig_2D
-default_params = {'TEMP':2.5, 'V_x':1, 'V_y':1.5, 'DELTA_T':0.002, 'TIMESTEPS':1000, 'BLOCK_SIZE':100,
-                  'CUTOFF_RADIUS':3, 'LINKED_CELL_SIZE_X':3, 'LINKED_CELL_SIZE_Y':3} 
 
 def generateConfig_2D(params):
-    with open('config2D_template.xml') as f:
-        template = f.read()
-    for param, val in default_params.items():
-        if param in params.keys():
-            val = params[param]
+    defaults = {'TEMP':2.5, 'V_x':1, 'V_y':1.5, 'DELTA_T':0.002, 'TIMESTEPS':1000, 'BLOCK_SIZE':100,
+                      'CUTOFF_RADIUS':3, 'LINKED_CELL_SIZE_X':3, 'LINKED_CELL_SIZE_Y':3} 
+    with open('config2D_template.xml') as f: template = f.read()
+    defaults.update(params)
+    for param, val in defaults.items():
         template = template.replace(param, str(val))
-    return template
+    return template, defaults
 
 def write_times(times, path = 'out.csv'):
     with open(path, 'w+') as f:
@@ -28,29 +26,22 @@ def write_times(times, path = 'out.csv'):
         writer.writerows(times)
         
 if __name__ == '__main__':
-    
-    times = []
-    cutoff_radius_range = np.linspace(0,5,100)
-    block_size_range = range(50,151, 5)
-    params = {'CUTOFF_RADIUS':cutoff_radius_range}
-    params = {'BLOCK_SIZE':block_size_range}
-    
     path = os.path.join(BASE_DIR, output_folder)
     if not os.path.exists(path): os.mkdir(path)
-    for param, param_range, in params.items():
-        for val in param_range:
-            config = generateConfig_2D({param:val})
-            filename = reduce(lambda s, param: '{0}_{1}={2:0.4f}'.format(s, param, val), 
-                              params, 'config2D') + '.xml'
-            path = os.path.join(BASE_DIR, output_folder, filename)
 
-            with open(path, 'w+') as f:
-                f.write(config)
+    times = []
+    param_space = {'CUTOFF_RADIUS':np.linspace(0.5, 1.5, 0.5), 'BLOCK_SIZE':range(85, 95, 5)}
+    for param, param_range, in param_space.items():
+        for val in param_range:
+            config, params = generateConfig_2D({param:val})
+            filename = reduce(lambda s, item: '{0}_{1}={2:0.4f}'.format(s, *item),
+                              params.items(), 'config2D') + '.xml'
+
+            path = os.path.join(BASE_DIR, output_folder, filename)
+            with open(path, 'w+') as f: f.write(config)
 
             start = time.time()
             res = subprocess.call(["./simplemd", os.path.join('..',path)])
-            if res == 0:
-                times.append((val, time.time() - start))
-            else:
-                times.append((val, 'NA'))
-    write_times(times, path = '{0}_times.csv'.format(param))
+            dt = time.time() - start if res == 0 else 'NA'
+            times.append((params, dt))
+    write_times(times, path = '{0}_times.csv'.format('_'.join(param_space.keys())))
