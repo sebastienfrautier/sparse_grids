61c5ba6cc80aa854b39db1cbf48556dd6dd0bc2e
kaibrusch <kaibrusch@gmail.com>
2017-12-12 01:01:19 +0100

    works, but missing density

--- configGenerator_2D.py
+++ configGenerator_2D.py
@@ -7,6 +7,7 @@ import numpy as np
 import subprocess
 import collections
 import random
+import sys
 
 BASE_DIR = os.getcwd()      # if we get amibitous, this could become a command line param
 output_folder = 'configs'   # this too
@@ -15,11 +16,14 @@ output_folder = 'configs'   # this too
 
 def generateConfig_2D(params):
     defaults = {'TEMP':2.5, 'V_x':1, 'V_y':1.5, 'DELTA_T':0.002, 'TIMESTEPS':1000, 'BLOCK_SIZE':100,
-                      'CUTOFF_RADIUS':3, 'LINKED_CELL_SIZE_X':3, 'LINKED_CELL_SIZE_Y':3}
+                      'CUTOFF_RADIUS':3, 'LINKED_CELL_SIZE_X':3, 'LINKED_CELL_SIZE_Y':3,'DOMAIN_SIZE':5,'MOL_X':2,'MOL_Y':2}
     with open('config2D_template.xml') as f: template = f.read()
     defaults.update(params)
     for param, val in defaults.items():
         template = template.replace(param, str(val))
+
+    for k,v in defaults.items():
+        print k,v
     return template, defaults
 
 def write_times(times, path = 'out.csv'):
@@ -32,51 +36,59 @@ if __name__ == '__main__':
     if not os.path.exists(path): os.mkdir(path)
 
     times = []
-    #param_space = {'LINKED_CELL_SIZE_Y':range(1,10,1),'LINKED_CELL_SIZE_X':range(1,10,1),'CUTOFF_RADIUS':np.linspace(0,1,10), 'BLOCK_SIZE':range(60, 95, 5), 'DOMAIN_SIZE':range(60, 95, 5)}
-    full_param_space = collections.OrderedDict({"molecules-per-direction": np.linspace(20, 80, 100)
-                                                   , 'LINKED_CELL_SIZE_Y': np.linspace(1, 20, 100)
-                                                   , 'LINKED_CELL_SIZE_X': np.linspace(1, 20, 100)
-                                                   , 'CUTOFF_RADIUS': np.linspace(0, 20, 100)
-                                                   , 'BLOCK_SIZE': range(2, 100)
-                                                   , 'DOMAIN_SIZE': range(1, 100)})
-    constrained_random_config = collections.OrderedDict({})
+    for i in range(0,int(sys.argv[1])):
 
-    for k, v in full_param_space.items():
+        full_param_space = collections.OrderedDict({"MOL_X": np.linspace(20, 80, 100)
+                                                        ,"MOL_Y": np.linspace(20, 80, 100)
+                                                        , 'LINKED_CELL_SIZE_Y': np.linspace(1, 20, 100)
+                                                        , 'LINKED_CELL_SIZE_X': np.linspace(1, 20, 100)
+                                                        , 'CUTOFF_RADIUS': np.linspace(0, 20, 100)
+                                                        , 'BLOCK_SIZE': range(2, 100)
+                                                        , 'DOMAIN_SIZE': range(1, 100)})
 
-        if k == 'BLOCK_SIZE':
+        constrained_random_config = collections.OrderedDict({})
 
-            block_size_choice = random.choice(full_param_space[k])
+        for k, v in full_param_space.items():
 
-            while (constrained_random_config['DOMAIN_SIZE'] % block_size_choice) != 0:
+            if k == 'BLOCK_SIZE':
+
                 block_size_choice = random.choice(full_param_space[k])
 
-            constrained_random_config[k] = block_size_choice
+                while (constrained_random_config['DOMAIN_SIZE'] % block_size_choice) != 0:
+                    block_size_choice = random.choice(full_param_space[k])
 
-        elif k == 'CUTOFF_RADIUS':
+                constrained_random_config[k] = block_size_choice
 
-            cutoff_radius_choice = random.choice(full_param_space[k])
-            while (constrained_random_config['LINKED_CELL_SIZE_X'] > cutoff_radius_choice and constrained_random_config[
-                'LINKED_CELL_SIZE_Y'] > cutoff_radius_choice):
+            elif k == 'CUTOFF_RADIUS':
+
                 cutoff_radius_choice = random.choice(full_param_space[k])
+                while (constrained_random_config['LINKED_CELL_SIZE_X'] < cutoff_radius_choice and constrained_random_config[
+                    'LINKED_CELL_SIZE_Y'] < cutoff_radius_choice):
+                    cutoff_radius_choice = random.choice(full_param_space[k])
 
-            constrained_random_config[k] = cutoff_radius_choice
+                constrained_random_config[k] = cutoff_radius_choice
 
-        else:
-            constrained_random_config[k] = random.choice(full_param_space[k])
+            else:
+                constrained_random_config[k] = random.choice(full_param_space[k])
 
 
-    for param, param_range, in constrained_random_config.items():
-        for val in param_range:
-            config, params = generateConfig_2D({param:val})
-            filename = reduce(lambda s, item: '{0}_{1}={2:0.4f}'.format(s, *item),
+        for param,val in constrained_random_config.items():
+            print param,val
+
+            #config, params = generateConfig_2D({param:val})
+
+        config, params = generateConfig_2D(constrained_random_config)
+        filename = reduce(lambda s, item: '{0}_{1}={2:0.4f}'.format(s, *item),
                               params.items(), 'config2D') + '.xml'
 
-            path = os.path.join(BASE_DIR, output_folder, filename)
-            with open(path, 'w+') as f: f.write(config)
+        path = os.path.join(BASE_DIR, output_folder, filename)
+        with open(path, 'w+') as f: f.write(config)
 
-            start = time.time()
+        start = time.time()
             #print os.path.join('..',path)
-            res = subprocess.call(["./simplemd", os.path.join('..',path)])
-            dt = time.time() - start if res == 0 else 'NA'
-            times.append((params, dt))
-    write_times(times, path = '{0}_times.csv'.format('_'.join(param_space.keys())))
+        res = subprocess.call(["./simplemd", os.path.join('..',path)])
+        dt = time.time() - start if res == 0 else 'NA'
+        times.append((params, dt))
+
+
+    write_times(times, path = '{0}_times.csv'.format('_'.join(constrained_random_config.keys())))
