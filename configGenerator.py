a1445aebc40ebf561c6cae7a971992f116c8110a
kaibrusch <kaibrusch@gmail.com>
2017-12-17 14:26:43 +0100

    output

--- configGenerator.py
+++ configGenerator.py
@@ -20,9 +20,9 @@ if __name__ == '__main__':
 
 
     problem.addVariables(["MOL_X", "MOL_Y"], range(40, 45))
-    problem.addVariables(["DOMAIN_SIZE_X", "DOMAIN_SIZE_Y"], np.arange(40,45,1))
-    problem.addVariables(['LINKED_CELL_SIZE_Y', 'LINKED_CELL_SIZE_X'], range(1,40,5))
-    problem.addVariables(["BLOCK_SIZE"], list([1,10,100,1000])) #logarithmic steps
+    problem.addVariables(["DOMAIN_SIZE_X", "DOMAIN_SIZE_Y"], np.arange(20,35,1))
+    problem.addVariables(['LINKED_CELL_SIZE_Y', 'LINKED_CELL_SIZE_X'], [0,5,10,15,20,25])
+    problem.addVariables(["BLOCK_SIZE"], list([1,10,100])) #logarithmic steps
     problem.addVariables(["CUTOFF_RADIUS"], np.arange(1.25,5,0.25))
 
 
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     # fix particle, aneder nur domain groesse --> immer noch mittel gross, 2k-3k
     # fix particle, irgendein cutoff, 1k steps, vtk rausschreiben, wann sind particle zufaellig verteilt
 
-    problem.addConstraint(lambda x, y, a, b: ((x * y) / (a * b)) < 2,
-                          ("MOL_X", "MOL_Y", "DOMAIN_SIZE_X", "DOMAIN_SIZE_Y"))
+    #problem.addConstraint(lambda x, y, a, b: ((x * y) / (a * b)) < 2,
+    #                      ("MOL_X", "MOL_Y", "DOMAIN_SIZE_X", "DOMAIN_SIZE_Y"))
 
     problem.addConstraint(lambda block, x, y: (x % block == 0) and (y % block == 0),
                           ("BLOCK_SIZE", "DOMAIN_SIZE_X", "DOMAIN_SIZE_Y"))  # you sure about this one? yes!
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     all_configs = problem.getSolutions()
 
     n=2000
-    name ="full_config_lists_full.1111"
+    name ="full_config_lists_full.XXX"
 
     samples=random.sample(all_configs,2000)
 
