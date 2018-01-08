5757e0a4216552dbfa1577e9af5cdb52df34dfb4
rob brown <rab170@pitt.edu>
2018-01-09 00:48:57 +0100

    little bit of clean up

--- runSimulation.py
+++ runSimulation.py
@@ -15,10 +15,12 @@ import string
 BASE_DIR = os.getcwd()      # if we get amibitous, this could become a command line param
 output_folder = 'configs'   # this too
 
+output_file = '2D_PARAMETER_SPACE.csv'
+
 def generateConfig_2D(params, checkpoint=None):
     # these are defautls which are used if certain parameters aren't defined for generateConfig_2D
     defaults = {'TEMP':2.5, 'V_x':1, 'V_y':1.5, 'DELTA_T':0.002, 'TIMESTEPS':1000, 'BLOCK_SIZE':100,
-                      'CUTOFF_RADIUS':3, 'LINKED_CELL_SIZE_X':3, 'LINKED_CELL_SIZE_Y':3,'DOMAIN_SIZE':5,'MOL_X':2,'MOL_Y':2}
+                      'CUTOFF_RADIUS':3, 'LINKED_CELL_SIZE_X':3, 'LINKED_CELL_SIZE_Y':3, 'MOL_X':2,'MOL_Y':2}
     defaults.update(params)
 
     if checkpoint:
@@ -42,13 +44,10 @@ def write_times(times, path = 'out.csv'):
 if __name__ == '__main__':
     path = os.path.join(BASE_DIR, output_folder)
     if not os.path.exists(path): os.mkdir(path)
-
     print 'get pickled config'
-
     full_config = pickle.load(open("configs_2D_param_space", "rb"))
 
-    n = 5
-
+    n = 100
     print 'running with %s configs' % n
     times = []
     for i in range(0, n):
@@ -57,21 +56,22 @@ if __name__ == '__main__':
 
         config = random.choice(full_config)
         filename = reduce(lambda s, item: '{0}_{1}={2}'.format(s, *item),
-                          this_config.items(), 'config2D') + '.xml'
+                          config.items(), 'config2D') + '.xml'
         path = os.path.join(BASE_DIR, output_folder, filename)
 
         config['CHECKPOINT'] = 'checkpoints/'+''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-        config, params = generateConfig_2D(config)
-        with open(path, 'w+') as f: f.write(config)
+        config_str, params = generateConfig_2D(config)
+        with open(path, 'w+') as f: f.write(config_str)
         res = subprocess.call(["./simplemd", os.path.join('..', path)])
 
         config['CHECKPOINT'] = config['CHECKPOINT']+'_999'
-        config, params = generateConfig_2D(config, True)
-        with open(path, 'w+') as f: f.write(config)
+        config_str, params = generateConfig_2D(config, True)
+        with open(path, 'w+') as f: f.write(config_str)
 
         start = time.time()
         res = subprocess.call(["./simplemd", os.path.join('..',path)])
         dt = time.time() - start if res == 0 else 'NA'
+
         times.append((params, dt))
 
-    write_times(times, path = '{0}_times.csv'.format('_'.join(full_config[i].keys())))
+    write_times(times, path = output_file)
